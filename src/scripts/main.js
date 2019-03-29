7dbc822605ce48c81cca5689db651bf5d39faf8e
sidomoon <babyenkoev@gmail.com>
2019-03-29 18:03:39 +0200

    done desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,40 @@
 $(document).ready(function () {
   /* focus on input */
-  $('.subscribe__input').focus(function () {
+  $('.subscribe__input')
+  .focus(function () {
     $('.subscribe__form').addClass('subscribe__form--focus');
-  }).blur(function () {
+  })
+  .blur(function () {
     $('.subscribe__form').removeClass('subscribe__form--focus');
   });
   /* /focus on input */
 
   /* input validation */
-  $(function () {
-    $('.subscribe__input').blur(function () {
-      if ($(this).is(':invalid')) {
-        $('.subscribe__form').addClass('subscribe__form--not-valid');
-      } else {
-        $('.subscribe__form').removeClass('subscribe__form--not-valid');
-      }
-    });
+  $('.subscribe__input').blur(function () {
+    if ($(this).is(':invalid')) {
+      $('.subscribe__form').addClass('subscribe__form--not-valid');
+    } else {
+      $('.subscribe__form').removeClass('subscribe__form--not-valid');
+    }
   });
   /* /input validation */
 
-  /* placeholder to top input */
-  $('.contact__input').focus(function () {
-    $(this).addClass('contact__input--focus');
-  }).blur(function () {
-    if ($(this).is(':invalid')) {
-      $(this).removeClass('contact__input--focus');
+  /* placeholder to top */
+  $('.contact__js').focus(function () {
+    $(this).addClass('contact__js--focus');
+  })
+  .blur(function () {
+    var value = $(this).val();
+    if (value === '') {
+      $(this).removeClass('contact__js--focus');
     }
   });
-  /* /placeholder to top input */
+  /* /placeholder to top */
 
-  /* placeholder to top textarea */
-  $('.contact__textarea').focus(function () {
-    $(this).addClass('contact__textarea--focus');
-  }).blur(function () {
-    if ($(this).is(':invalid')) {
-      $(this).removeClass('contact__textarea--focus');
-    }
+  /* masonry */
+  $('.media__list').masonry({
+    itemSelector: '.media__item',
+    columnWidth: 500
   });
-  /* /placeholder to top textarea */
+  /* /masonry */
 });
