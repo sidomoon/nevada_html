33602ab2438d6081b5b4740594e57daffa9869f2
sidomoon <babyenkoev@gmail.com>
2019-04-01 18:06:47 +0300

    responsive main page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,8 +33,26 @@ $(document).ready(function () {
 
   /* masonry */
   $('.media__list').masonry({
-    itemSelector: '.media__item',
-    columnWidth: 500
-  });
+  itemSelector: '.media__item',
+  columnWidth: 500
+});
   /* /masonry */
+
+  /* menu toggle */
+  var menu = document.querySelector('.page-header__menu'),
+      toggle = document.querySelector('.page-header__toggle');
+
+  menu.classList.remove('page-header__menu--open');
+  menu.classList.add('page-header__menu--closed');
+
+  toggle.addEventListener('click', function() {
+    if (menu.classList.contains('page-header__menu--closed')) {
+      menu.classList.remove('page-header__menu--closed');
+      menu.classList.add('page-header__menu--open');
+    } else {
+      menu.classList.remove('page-header__menu--open');
+      menu.classList.add('page-header__menu--closed');
+    }
+  });
+  /* /menu toggle */
 });
